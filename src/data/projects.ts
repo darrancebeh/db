6f120d30d9ec6949096990abfb9a6d6ccc2f5d8b
darrancebeh <darrancebeh@gmail.com>
2025-04-29 17:17:35 +0800

    add: Added Monte Carlol project into portfolio.

--- src/data/projects.ts
+++ src/data/projects.ts
@@ -28,19 +28,19 @@ export const projects: Project[] = [
   },
   {
     id: 3,
+    title: 'Monte Carlol',
+    imageUrl: '/images/project-montecarlol-screenshot.png',
+    description: 'Monte-carlol is an interactive Monte Carlo simulation for stock price forecasting with real-time visualization. Preview Hosting Soon.',
+    techStack: ['Next.js', 'React', 'FastAPI', 'Pydantic', 'Recharts', 'Tailwind CSS'],
+    githubUrl: 'https://github.com/darrancebeh/monte-carlol',
+  },
+   {
+    id: 4,
     title: 'Word-4-Word',
     imageUrl: '/images/project-word4word-screenshot.png',
     description: 'Word-4-Word is a web-based OCR application that recognizes text from handwriting drawn by users on a canvas. Preview Hosting Soon.',
     techStack: ['Next.js', 'React', 'FastAPI', 'PaddleOCR', 'Pillow', 'Tailwind CSS'],
     githubUrl: 'https://github.com/darrancebeh/word4word',
-  },
-   {
-    id: 4,
-    title: 'Placeholder project',
-    imageUrl: '/images/project4_placeholder.png',
-    description: 'Lorem ipsum bridge is falling down.',
-    techStack: ['lorem', 'ipsum', 'html CSS'],
-    githubUrl: 'https://github.com/darrancebeh',
   },
   {
     id: 5,
